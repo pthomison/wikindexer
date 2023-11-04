4525d21755eb2a1791cc780aedc9db792e2b1982
pthomison <p.thomison@gmail.com>
2023-11-04 15:38:45 -0700

    v2

--- main.go
+++ main.go
@@ -1,48 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"time"
+	"os"
+
+	_ "github.com/glebarez/go-sqlite"
+	"github.com/hashicorp/go-retryablehttp"
+	"github.com/pthomison/errcheck"
+	"gitlab.com/tozd/go/errors"
+	"gitlab.com/tozd/go/mediawiki"
 )
 
-// import (
-// 	"context"
-// 	"fmt"
+// c := db.NewClient(":memory:")
 
-// 	_ "github.com/glebarez/go-sqlite"
-// 	"github.com/hashicorp/go-retryablehttp"
-// 	"github.com/pthomison/errcheck"
-// 	"gitlab.com/tozd/go/errors"
-// 	"gitlab.com/tozd/go/mediawiki"
-// )
+// articles := []wikindexer.Article{}
 
-// func main() {
-// 	ctx := context.TODO()
-// 	retryableClient := retryablehttp.NewClient()
-
-// 	// c := db.NewClient(":memory:")
-
-// 	// articles := []wikindexer.Article{}
-
-// 	// c.DB.Select(&articles, "SELECT * FROM place ORDER BY telcode ASC")
-
-// 	url, err := mediawiki.LatestWikipediaRun(ctx, retryableClient, "enwiki", 0)
-// 	errcheck.Check(err)
-
-// 	mediawiki.ProcessWikipediaDump(ctx, &mediawiki.ProcessDumpConfig{
-// 		URL:    url,
-// 		Path:   "./.wikicache",
-// 		Client: retryableClient,
-// 	}, func(ctx context.Context, a mediawiki.Article) errors.E {
-// 		fmt.Printf("%v\n", a.Name)
-// 		return nil
-// 	})
-
-// }
+// c.DB.Select(&articles, "SELECT * FROM place ORDER BY telcode ASC")
 
 func main() {
-	for {
-		fmt.Println("hello world")
-		time.Sleep(1 * time.Second)
-	}
+	ctx := context.TODO()
+	retryableClient := retryablehttp.NewClient()
+
+	cacheDirPath := fmt.Sprintf("%v/.config/wikindexer", os.Getenv("HOME"))
+	cacheFilePath := fmt.Sprintf("%v/cache", cacheDirPath)
+
+	err := os.MkdirAll(cacheDirPath, 0755)
+	errcheck.Check(err)
+
+	url, err := mediawiki.LatestWikipediaRun(ctx, retryableClient, "enwiki", 0)
+	errcheck.Check(err)
+
+	mediawiki.ProcessWikipediaDump(ctx, &mediawiki.ProcessDumpConfig{
+		URL:    url,
+		Path:   cacheFilePath,
+		Client: retryableClient,
+	}, func(ctx context.Context, a mediawiki.Article) errors.E {
+		fmt.Printf("%v\n", a.Name)
+		return nil
+	})
+
 }
+
+// func main() {
+// 	for {
+// 		fmt.Println("hello world")
+// 		time.Sleep(1 * time.Second)
+// 	}
+// }
